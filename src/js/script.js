2c8c60d45fd8042e743f9eb5b4d8bcd0739a372f
matheusmmorais <matheusmarcelinodemorais@gmail.com>
2026-04-28 11:20:06 -0300

    Feat: Metodos trim, split, includes, join

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+// LIMPEZA
+
+const email="       email@email.com   ";
+//trim- remove os espaçoes Início e fim
+const emailLimpo= email.trim();
+console.log(emailLimpo)
+
+//VALIDAÇÃO
+if(emailLimpo.includes("@")){
+    console.log("Email válido")
+}else{
+    console.log("Email inválido")
+}
+
+//TRANFORMAÇÃO DE TEXTO
+
+const tituloArtigo="Como aprender JAVA RAIZ";
+//metodo upper(maiúsculo) e low(minúsculo)
+const texto1 = tituloArtigo.toLowerCase();
+console.log(texto1)
+//split- transfroma o texto em array
+const texto2 =tituloArtigo.split(" ")
+console.log(texto2)
+//join- junta tudo em uma unica string 
+const texto3 = texto2.join(" ")
+console.log(texto3)
